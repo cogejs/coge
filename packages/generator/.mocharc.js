5002a50eec3efd29c87f31c0ebc1a9accea29fe8
TY <towyuan@outlook.com>
2023-07-02 14:30:49 +0800

    chore: cleanup

--- packages/generator/.mocharc.js
+++
@@ -1,8 +0,0 @@
-const {mergeMochaConfigs} = require('@nutol/build');
-const defaultConfig = require('@nutol/build/config/.mocharc.json');
-
-const MONOREPO_CONFIG = {
-  parallel: true,
-};
-
-module.exports = mergeMochaConfigs(defaultConfig, MONOREPO_CONFIG);
